ccefd24d9038454e165180dd9209b85b9aba73f6
Anirudhan J. Rajagopalan <ajr619@nyu.edu>
2016-04-27 14:06:24 -0400

    Minor changes for getting our own outbound urls

--- src/main/java/com/reviewhashed/crawler/ReviewsParser.java
+++ src/main/java/com/reviewhashed/crawler/ReviewsParser.java
@@ -1,23 +1,44 @@
 package com.reviewhashed.crawler;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.html.HtmlParser;
+import org.xml.sax.ContentHandler;
+
 import edu.uci.ics.crawler4j.crawler.CrawlConfig;
 import edu.uci.ics.crawler4j.crawler.Page;
 import edu.uci.ics.crawler4j.crawler.exceptions.ParseException;
+import edu.uci.ics.crawler4j.parser.HtmlContentHandler;
 import edu.uci.ics.crawler4j.parser.NotAllowedContentException;
 import edu.uci.ics.crawler4j.parser.Parser;
 
 public class ReviewsParser extends Parser {
+	
+	private final HtmlParser htmlParser;
+	private final ParseContext parseContext;
 
 	public ReviewsParser(CrawlConfig config) {
 		super(config);
+		htmlParser = new HtmlParser();
+		parseContext = new ParseContext();
 		System.out.println("calling this config");
 	}
 	
 	@Override
 	public void parse(Page page, String contextURL) throws NotAllowedContentException, ParseException {
-//		super.parse(page, contextURL);
 		System.out.println("Parse function of child");
-		System.exit(0);
+		Metadata metadata = new Metadata();
+		ContentHandler contentHandler = new ReviewContentHandler();
+		
+		try (InputStream inputStream = new ByteArrayInputStream(page.getContentData())) {
+	        htmlParser.parse(inputStream, contentHandler, metadata, parseContext);
+	      } catch (Exception e) {
+	        logger.error("{}, while parsing: {}", e.getMessage(), page.getWebURL().getURL());
+	        throw new ParseException();
+	      }
 	}
 
 }
