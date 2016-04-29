1e4bc0aee523d643bf67267a3ccd42877747d077
Anirudhan J. Rajagopalan <ajr619@nyu.edu>
2016-04-29 16:54:28 -0400

    Changes for logging a few details for seed urls.

--- src/main/java/com/reviewhashed/crawler/ReviewsController.java
+++ src/main/java/com/reviewhashed/crawler/ReviewsController.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+import org.slf4j.Logger;
 
 import au.com.bytecode.opencsv.CSVReader;
 import edu.uci.ics.crawler4j.crawler.CrawlConfig;
@@ -13,66 +14,66 @@ import edu.uci.ics.crawler4j.crawler.CrawlController;
 import edu.uci.ics.crawler4j.fetcher.PageFetcher;
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+import uk.org.lidalia.slf4jext.LoggerFactory;
 
 public class ReviewsController {
 
-  @Parameter(names = { "-location", "-l" }, required = true)
-  private static String storageLocation;
+	private static final Logger logger = LoggerFactory.getLogger(ReviewsController.class);
 
-  @Parameter(names = { "-seedUrls", "-s" }, required = true)
-  private String seedUrlsFile;
+	@Parameter(names = { "-location", "-l" }, required = true)
+	private static String storageLocation;
 
-  @Parameter(names = { "-ncrawl", "-n" }, required = true)
-  private int numCrawlers;
-  
-  @Parameter(names = { "-crawlData", "-c" }, required = true)
-  private static String crawlData;
-  
-  
+	@Parameter(names = { "-seedUrls", "-s" }, required = true)
+	private String seedUrlsFile;
 
-  private String getReviewUrl(String asin, int pageNum) {
-    return "http://www.amazon.com/asdf/product-reviews/"+asin+"/ref=undefined_2?filterbystar=one_star&pageNumber=1";
-    //		return "http://www.amazon.com/product-reviews/" + asin + "/ref=undefined_" + pageNum + "?pageNumber=" + pageNum;
-  }
+	@Parameter(names = { "-ncrawl", "-n" }, required = true)
+	private int numCrawlers;
 
+	@Parameter(names = { "-crawlData", "-c" }, required = true)
+	private static String crawlData;
 
-  private void startCrawl() throws Exception {
-    CrawlConfig config = new CrawlConfig();
-    config.setCrawlStorageFolder(storageLocation);
-    config.setUserAgentString("Reviewrehash - http://cs.nyu.edu/~ajr619/crawlbot.html");
-    config.setIncludeBinaryContentInCrawling(true);
+	private String getReviewUrl(String asin, int pageNum) {
+		return "http://www.amazon.com/asdf/product-reviews/" + asin;
+	}
 
-    PageFetcher pageFetcher = new PageFetcher(config);
-    RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
-    RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
-    CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+	private void startCrawl() throws Exception {
+		CrawlConfig config = new CrawlConfig();
+		config.setCrawlStorageFolder(storageLocation);
+		config.setUserAgentString("Reviewrehash - http://cs.nyu.edu/~ajr619/crawlbot.html");
+		config.setIncludeBinaryContentInCrawling(true);
 
-    Set<String> allowedUrls = new HashSet<>();
+		PageFetcher pageFetcher = new PageFetcher(config);
+		RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+		RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+		CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
 
-    CSVReader reader = new CSVReader(new FileReader(this.seedUrlsFile), ',','"');
-    String[] newLine;
+		Set<String> allowedUrls = new HashSet<>();
 
-    while((newLine = reader.readNext()) != null){
-      String asin = newLine[0];
-      String seedUrl = getReviewUrl(asin,1);
-      controller.addSeed(seedUrl);
-      allowedUrls.add(asin.toLowerCase());
-    }
-    reader.close();
-    ReviewsCrawler.configure(storageLocation,crawlData);
-    ReviewsCrawler.setAllowedUrls(allowedUrls);
+		CSVReader reader = new CSVReader(new FileReader(this.seedUrlsFile), ',', '"');
+		String[] newLine;
 
-    controller.start(ReviewsCrawler.class, this.numCrawlers);
-  }
+		while ((newLine = reader.readNext()) != null) {
+			String asin = newLine[0];
+			String seedUrl = getReviewUrl(asin, 1);
+			logger.info("Adding seedurl " + seedUrl);
+			controller.addSeed(seedUrl);
+			allowedUrls.add(asin.toLowerCase());
+		}
+		reader.close();
+		ReviewsCrawler.configure(storageLocation, crawlData);
+		ReviewsCrawler.setAllowedUrls(allowedUrls);
 
-  public static void main(String[] args) {
-    ReviewsController c = new ReviewsController();
-    new JCommander(c, args);
-    try {
-      c.startCrawl();
-    } catch (Exception e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-  }
+		controller.start(ReviewsCrawler.class, this.numCrawlers);
+	}
+
+	public static void main(String[] args) {
+		ReviewsController c = new ReviewsController();
+		new JCommander(c, args);
+		try {
+			c.startCrawl();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
--- src/main/java/com/reviewhashed/crawler/ReviewsCrawler.java
+++ src/main/java/com/reviewhashed/crawler/ReviewsCrawler.java
@@ -24,115 +24,114 @@ import edu.uci.ics.crawler4j.url.WebURL;
 import uk.org.lidalia.slf4jext.LoggerFactory;
 
 public class ReviewsCrawler extends WebCrawler {
-  private static final Logger logger = LoggerFactory.getLogger(ReviewsCrawler.class);
-  private static File storageFolder;
-  private static File crawlDataFolder;
-  private static Set<String> allowedASINs;
+	private static final Logger logger = LoggerFactory.getLogger(ReviewsCrawler.class);
+	private static File storageFolder;
+	private static File crawlDataFolder;
+	private static Set<String> allowedASINs;
 
-  @Override
-  public void init(int id, CrawlController crawlController) {
-    super.init(id, crawlController);
-    try {
-      Field declaredField = this.getClass().getSuperclass().getDeclaredField("parser");
-      declaredField.setAccessible(true);
-      declaredField.set(this, new ReviewsParser(crawlController.getConfig()));
+	@Override
+	public void init(int id, CrawlController crawlController) {
+		super.init(id, crawlController);
+		try {
+			Field declaredField = this.getClass().getSuperclass().getDeclaredField("parser");
+			declaredField.setAccessible(true);
+			declaredField.set(this, new ReviewsParser(crawlController.getConfig()));
 
-    } catch (NoSuchFieldException | SecurityException e) {
-      e.printStackTrace();
-    } catch (IllegalArgumentException e) {
-      e.printStackTrace();
-    } catch (IllegalAccessException e) {
-      e.printStackTrace();
-    }
+		} catch (NoSuchFieldException | SecurityException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
 
-  }
+	}
 
-  public static void configure(String storageFolderName, String crawlDataLocation) {
+	public static void configure(String storageFolderName, String crawlDataLocation) {
 
-    storageFolder = new File(storageFolderName);
-    if (!storageFolder.exists()) {
-      storageFolder.mkdirs();
-    }
+		storageFolder = new File(storageFolderName);
+		if (!storageFolder.exists()) {
+			storageFolder.mkdirs();
+		}
 
-    crawlDataFolder = new File(crawlDataLocation);
-    if (!crawlDataFolder.exists()) {
-      crawlDataFolder.mkdirs();
-    }
-  }
+		crawlDataFolder = new File(crawlDataLocation);
+		if (!crawlDataFolder.exists()) {
+			crawlDataFolder.mkdirs();
+		}
+	}
 
-  public static void setAllowedUrls(Set<String> allowedUrls) {
-    allowedASINs = allowedUrls;
-  }
+	public static void setAllowedUrls(Set<String> allowedUrls) {
+		allowedASINs = allowedUrls;
+	}
 
-  @Override
-  public boolean shouldVisit(Page referringPage, WebURL url) {
+	@Override
+	public boolean shouldVisit(Page referringPage, WebURL url) {
 
-    String urlString = url.getURL().toLowerCase();
-    String asin = urlString.substring(urlString.lastIndexOf("/") + 1,
-        urlString.indexOf('?') == -1 ? urlString.length() : urlString.indexOf('?')).toLowerCase();
+		String urlString = url.getURL().toLowerCase();
+		String asin = urlString.substring(urlString.lastIndexOf("/") + 1,
+				urlString.indexOf('?') == -1 ? urlString.length() : urlString.indexOf('?')).toLowerCase();
 
-    if (allowedASINs.contains(asin)) {
-      logger.info("Returning True " +url.getURL().toLowerCase() + " " + asin);
-      return true;
-    }
-    logger.info("Returning false " + url.getURL().toLowerCase() + " " + asin);
-    return false;
-  }
+		if (allowedASINs.contains(asin)) {
+			logger.info("Returning True " + url.getURL().toLowerCase() + " " + asin);
+			return true;
+		}
+		logger.info("Returning false " + url.getURL().toLowerCase() + " " + asin);
+		return false;
+	}
 
-  @Override
-  public void visit(Page page) {
+	@Override
+	public void visit(Page page) {
 
-    //String review = getReviewInfo(page);
+		// String review = getReviewInfo(page);
 
-    
-    String urlString = page.getWebURL().getURL();
-    int pageNumber = 0;
-    logger.info("Visiting : " + urlString);
-    try {
-      List<NameValuePair> parse = URLEncodedUtils.parse(new URI(urlString), "UTF-8");
-      for (NameValuePair nvPair : parse) {
-        if("pagenumber".equalsIgnoreCase(nvPair.getName())){;
-          pageNumber = Integer.parseInt(nvPair.getValue());
-        }
-      }
-    } catch (URISyntaxException e1) {
-      e1.printStackTrace();
-    }
-    String asin = urlString.substring(urlString.lastIndexOf("/") + 1,
-        urlString.indexOf('?') == -1 ? urlString.length() : urlString.indexOf('?')).toLowerCase();
-    
-    File folder = new File(crawlDataFolder+"/"+ asin + "/reviews" );
-    if (!folder.exists()) {
-      folder.mkdirs();
-    }
-    FileOutputStream fos = null;
-    try {
-      logger.info("Visited page now"+ page.getWebURL());
-      File file = new File(folder.getPath()+"/"+pageNumber+".html");
-      fos = new FileOutputStream(file);
-      fos.write(page.getContentData());
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    finally{
-      try{
-        if (fos != null){
-          fos.close();
-        }
-      }catch (IOException ioe) {
-        logger.error("Error while closing stream: " + ioe);
-      }
-    }
-  }
+		String urlString = page.getWebURL().getURL();
+		int pageNumber = 0;
+		logger.info("Visiting : " + urlString);
+		try {
+			List<NameValuePair> parse = URLEncodedUtils.parse(new URI(urlString), "UTF-8");
+			for (NameValuePair nvPair : parse) {
+				if ("pagenumber".equalsIgnoreCase(nvPair.getName())) {
+					pageNumber = Integer.parseInt(nvPair.getValue());
+				}
+			}
+		} catch (URISyntaxException e1) {
+			logger.error(e1.getMessage());
+			e1.printStackTrace();
+		}
+		String asin = urlString.substring(urlString.lastIndexOf("/") + 1,
+				urlString.indexOf('?') == -1 ? urlString.length() : urlString.indexOf('?')).toLowerCase();
 
-  private String getReviewInfo(File page) throws IOException{
-    Document doc = Jsoup.parse(page,"UTF-8","");
-    Elements features = doc.select("div#feature-bullets").select("li:not(.aok-hidden");
-    StringBuilder sb = new StringBuilder();
-    for(Element feature : features){
-      sb.append(feature.text());
-      sb.append("\n");
-    }
-    return sb.toString();
-  }
+		File folder = new File(crawlDataFolder + "/" + asin + "/reviews");
+		if (!folder.exists()) {
+			folder.mkdirs();
+		}
+		FileOutputStream fos = null;
+		try {
+			// logger.info("Visited page now"+ page.getWebURL());
+			File file = new File(folder.getPath() + "/" + pageNumber + ".html");
+			fos = new FileOutputStream(file);
+			fos.write(page.getContentData());
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (fos != null) {
+					fos.close();
+				}
+			} catch (IOException ioe) {
+				logger.error("Error while closing stream: " + ioe);
+			}
+		}
+	}
+
+	private String getReviewInfo(File page) throws IOException {
+		Document doc = Jsoup.parse(page, "UTF-8", "");
+		Elements features = doc.select("div#feature-bullets").select("li:not(.aok-hidden");
+		StringBuilder sb = new StringBuilder();
+		for (Element feature : features) {
+			sb.append(feature.text());
+			sb.append("\n");
+		}
+		return sb.toString();
+	}
 }
