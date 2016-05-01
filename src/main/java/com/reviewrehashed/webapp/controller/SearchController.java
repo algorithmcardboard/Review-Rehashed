71bd2f7335861c0453b7a92955feae0b6978f0f6
Anirudhan J. Rajagopalan <ajr619@nyu.edu>
2016-04-30 23:17:35 -0400

    Changes for querying index.

--- src/main/java/com/reviewrehashed/webapp/controller/SearchController.java
+++ src/main/java/com/reviewrehashed/webapp/controller/SearchController.java
@@ -3,33 +3,59 @@ package com.reviewrehashed.webapp.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.lucene.document.Document;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.gson.Gson;
+import com.reviewrehashed.indexer.HTMLParser;
 import com.reviewrehashed.webapp.dto.SearchResult;
+import com.reviewrehashed.webapp.dto.SearchResult.Builder;
 import com.reviewrehashed.webapp.service.RetrieverService;
 
+import edu.uci.ics.crawler4j.parser.HtmlParseData;
 import uk.org.lidalia.slf4jext.LoggerFactory;
 
 @Controller
 public class SearchController {
 
 	private static final Logger logger = LoggerFactory.getLogger(SearchController.class);
+	private Gson gson = new Gson();
 
-  // Tells the application context to inject an instance of UserService here
-  @Autowired
-  private RetrieverService retrieverService;
-	
+	// Tells the application context to inject an instance of UserService here
+	@Autowired
+	private RetrieverService retrieverService;
+
 	@ResponseBody
 	@RequestMapping("search.do")
-	public List<SearchResult> search(@RequestParam("feature") String feature, @RequestParam("product") String product) {
+	public ResponseEntity<String> search(@RequestParam("feature") String feature,
+			@RequestParam("product") String product) {
 		List<SearchResult> searchResults = new ArrayList<>();
 
-		logger.info("Feature is ", feature, " product is ", product);
-		return searchResults;
+		List<Document> results = new ArrayList<>();
+		try {
+			results = retrieverService.search(feature, product);
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.info("Error ", e);
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		System.out.println("Results size is " + results.size());
+		for (Document doc : results) {
+			Builder b = new Builder();
+			b.addAsin(doc.get(HTMLParser.ASIN)).addProductTitle(doc.get(HTMLParser.PRODUCT_TITLE))
+					.addReviewID(doc.get(HTMLParser.REVIEW_ID));
+			searchResults.add(b.build());
+		}
+
+		String json = gson.toJson(searchResults);
+		return new ResponseEntity<String>(json, HttpStatus.CREATED);
 	}
 }
--- src/main/java/com/reviewrehashed/webapp/service/RetrieverService.java
+++ src/main/java/com/reviewrehashed/webapp/service/RetrieverService.java
@@ -1,8 +1,11 @@
 package com.reviewrehashed.webapp.service;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.search.BooleanClause;
@@ -22,58 +25,49 @@ import com.reviewrehashed.indexer.HTMLParser;
 @Service
 public class RetrieverService {
 
-  private String indexDirPath = "D:/JAVA Workspace/Review-Rehashed/build/libs/luceneIndex";
+	private String indexDirPath = "D:/JAVA Workspace/Review-Rehashed/build/libs/luceneIndex";
 
-  public void start(String featureQuery, String productQuery) throws Exception {
+	public List<Document> search(String featureQuery, String productQuery) throws Exception {
 
-    File indexDir = new File(indexDirPath);
+		File indexDir = new File(indexDirPath);
+		List<Document> resultDocs = new ArrayList<>();
 
-    if (!indexDir.exists() || !indexDir.isDirectory()) {
-      throw new Exception(indexDir + " does not exist or is not a directory.");
-    }
-    search(indexDir, productQuery, featureQuery);
-  }
+		if (!indexDir.exists() || !indexDir.isDirectory()) {
+			throw new Exception(indexDir + " does not exist or is not a directory.");
+		}
 
-  public void search(File indexDir, String productQuery, String featureQuery) throws Exception {
-    Directory fsDir = FSDirectory.open(indexDir.toPath());
-    IndexSearcher is = new IndexSearcher(DirectoryReader.open(fsDir));
+		Directory fsDir = FSDirectory.open(indexDir.toPath());
+		IndexSearcher is = new IndexSearcher(DirectoryReader.open(fsDir));
 
-    Query productQuery1 = new TermQuery(new Term(HTMLParser.REVIEW_TITLE, productQuery));
-    Query productQuery2 = new TermQuery(new Term(HTMLParser.PRODUCT_TITLE, productQuery));
-    Query productQuery3 = new TermQuery(new Term(HTMLParser.REVIEW_CONTENT, productQuery));
+		Query productQuery1 = new TermQuery(new Term(HTMLParser.REVIEW_TITLE, productQuery));
+		Query productQuery2 = new TermQuery(new Term(HTMLParser.PRODUCT_TITLE, productQuery));
+		Query productQuery3 = new TermQuery(new Term(HTMLParser.REVIEW_CONTENT, productQuery));
 
-    Builder productQueryBuilder = new BooleanQuery.Builder();
-    BooleanQuery booleanProductQuery = productQueryBuilder
-        .add(productQuery1, BooleanClause.Occur.SHOULD)
-        .add(productQuery2, BooleanClause.Occur.SHOULD)
-        .add(productQuery3, BooleanClause.Occur.SHOULD)
-        .build();
+		Builder productQueryBuilder = new BooleanQuery.Builder();
+		BooleanQuery booleanProductQuery = productQueryBuilder.add(productQuery1, BooleanClause.Occur.SHOULD)
+				.add(productQuery2, BooleanClause.Occur.SHOULD).add(productQuery3, BooleanClause.Occur.SHOULD).build();
 
-    Query featureQuery1 = new TermQuery(new Term(HTMLParser.REVIEW_CONTENT, featureQuery));
-    Query featureQuery2 = new TermQuery(new Term(HTMLParser.REVIEW_TITLE, featureQuery));
-    
-    Builder featureQueryBuilder = new BooleanQuery.Builder();
-    BooleanQuery booleanFeatureQuery = featureQueryBuilder
-        .add(featureQuery1, BooleanClause.Occur.SHOULD)
-        .add(featureQuery2, BooleanClause.Occur.SHOULD)
-        .build();
+		Query featureQuery1 = new TermQuery(new Term(HTMLParser.REVIEW_CONTENT, featureQuery));
+		Query featureQuery2 = new TermQuery(new Term(HTMLParser.REVIEW_TITLE, featureQuery));
 
-    Builder queryBuilder = new BooleanQuery.Builder();
-    BooleanQuery booleanQuery = queryBuilder
-        .add(booleanProductQuery, BooleanClause.Occur.MUST)
-        .add(booleanFeatureQuery, BooleanClause.Occur.MUST)
-        .build();
+		Builder featureQueryBuilder = new BooleanQuery.Builder();
+		BooleanQuery booleanFeatureQuery = featureQueryBuilder.add(featureQuery1, BooleanClause.Occur.SHOULD)
+				.add(featureQuery2, BooleanClause.Occur.SHOULD).build();
 
-    long start = new Date().getTime();
-    TopDocs hits = is.search(booleanQuery,1);
-    long end = new Date().getTime();
-    System.err.println("Found " + hits.totalHits + " document(s) (in " + (end - start)
-        + " milliseconds) that matched query '" + productQuery + ": " + featureQuery + "':");
+		Builder queryBuilder = new BooleanQuery.Builder();
+		BooleanQuery booleanQuery = queryBuilder.add(booleanProductQuery, BooleanClause.Occur.MUST)
+				.add(booleanFeatureQuery, BooleanClause.Occur.MUST).build();
 
-    for (int i = 0; i < hits.scoreDocs.length; i++) {
-      ScoreDoc scoreDoc = hits.scoreDocs[i];
-      org.apache.lucene.document.Document doc = is.doc(scoreDoc.doc);
-      System.out.println(doc.getField(HTMLParser.REVIEW_CONTENT).stringValue());
-    }
-  }
+		long start = new Date().getTime();
+		TopDocs hits = is.search(booleanQuery, 1);
+		long end = new Date().getTime();
+		System.err.println("Found " + hits.totalHits + " document(s) (in " + (end - start)
+				+ " milliseconds) that matched query '" + productQuery + ": " + featureQuery + "':");
+
+		for (int i = 0; i < hits.scoreDocs.length; i++) {
+			ScoreDoc scoreDoc = hits.scoreDocs[i];
+			resultDocs.add(is.doc(scoreDoc.doc));
+		}
+		return resultDocs;
+	}
 }
